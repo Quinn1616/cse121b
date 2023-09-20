213e6a2e25c644aa40eee4934b97f71c7c2f868a
Quinn Bastian <jacobqbastian@gmail.com>
2023-09-20 15:48:00 -0600

    W02 Work

---
+++ scripts/w02-task.js
@@ -0,0 +1,33 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = "Quinn Bastian";
+var currentYear = "2023";
+var profilePicture = "images/Quinn.jpg"
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+
+var imageElement = document.querySelector('img[src="images/placeholder.png"]');
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
